c40905de07e0f6ede53fabe371fcb808aabd12db
André Ricardo <contatoadmx@gmail.com>
2025-06-08 14:34:33 -0300

    defulat DEV false

--- helper/script/env.php
+++ helper/script/env.php
@@ -5,7 +5,7 @@ use PhpMx\Env;
 Env::loadFile('./.env');
 Env::loadFile('./.conf');
 
-Env::default('DEV', true);
+Env::default('DEV', false);
 
 Env::default('CIF', 'base');
 Env::default('CODE', 'codekey');
