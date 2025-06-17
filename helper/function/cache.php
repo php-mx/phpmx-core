8de68acf879d30d9de1ee95da076f23cd7e0dad7
AndreRicardoé <contatoadmx@gmail.com>
2025-06-17 18:09:29 -0300

    cache file

---
+++ helper/function/cache.php
@@ -0,0 +1,33 @@
+<?php
+
+use PhpMx\Json;
+
+if (!function_exists('cache')) {
+
+    /** Armazena e recupera o retorno de uma Closure em /storage/cache */
+    function cache(string $cacheName, Closure $action): mixed
+    {
+        $file = path('storage/cache', strToSnakeCase($cacheName));
+
+        $result = Json::import($file);
+
+        if (!env('DEV') && !empty($result))
+            return array_shift($result);
+
+        try {
+            $result = $action();
+        } catch (Throwable $e) {
+            throw $e;
+        }
+
+        if (is_closure($result))
+            return $result;
+
+        try {
+            Json::export([$result], $file);
+        } catch (Throwable) {
+        }
+
+        return $result;
+    }
+}
