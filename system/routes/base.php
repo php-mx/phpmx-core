fc2ca57a0dffcdb79367fd04b4df23d8d3eb30ea
André Ricardo <contatoadmx@gmail.com>
2025-07-19 16:04:10 -0300

    full class in router

--- system/routes/base.php
+++ system/routes/base.php
@@ -1,18 +1,16 @@
 <?php
 
-namespace Controller;
-
 use PhpMx\Router;
 
-Router::get('assets/...', Base\Assets::class);
-Router::get('download/...', Base\Download::class);
+Router::get('assets/...', \Controller\Base\Assets::class);
+Router::get('download/...', \Controller\Base\Download::class);
 
-Router::get('favicon.ico', Base\Favicon::class);
-Router::get('robots.txt', Base\Robots::class);
-Router::get('sitemap.xml', Base\Sitemap::class);
-Router::get('style.css', Base\Style::class);
-Router::get('script.js', Base\Script::class);
+Router::get('favicon.ico', \Controller\Base\Favicon::class);
+Router::get('robots.txt', \Controller\Base\Robots::class);
+Router::get('sitemap.xml', \Controller\Base\Sitemap::class);
+Router::get('style.css', \Controller\Base\Style::class);
+Router::get('script.js', \Controller\Base\Script::class);
 
-Router::get('captcha', Base\Captcha::class);
+Router::get('captcha', \Controller\Base\Captcha::class);
 
 Router::get('', STS_OK);
