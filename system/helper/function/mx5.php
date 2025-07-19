fd4da7ac3c7b784a63a5be2444fb65e526785555
André Ricardo <contatoadmx@gmail.com>
2025-07-19 16:01:30 -0300

    fx mx5 helper

--- system/helper/function/mx5.php
+++ system/helper/function/mx5.php
@@ -5,7 +5,7 @@ use PhpMx\Mx5;
 if (!function_exists('mx5')) {
 
     /** Retorna o mx5 de uma variável */
-    function mx5(mixed $var): bool
+    function mx5(mixed $var): mixed
     {
         return Mx5::on($var);
     }
