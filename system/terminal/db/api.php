0fff58cd6f70bcc7312b84e3e8be07a620c308c6
André Ricardo <contatoadmx@gmail.com>
2025-07-23 19:23:17 -0300

    mx db.api

--- system/terminal/db/api.php
+++ system/terminal/db/api.php
@@ -42,17 +42,89 @@ return new class extends Terminal {
         $className = strToPascalCase($tableName);
         $model = strToPascalCase('db ' . $this->dbName);
 
+        $scheme = [];
+        $inputCreate = [];
+        $inputUpdate = [];
+
+        foreach ($tableMap['fields'] as $fieldName => $fieldMap) {
+            if ($fieldMap['type'] != 'md5' && $fieldMap['type'] != 'mx5') {
+                if ($fieldMap['type'] == 'idx') {
+                    $scheme[] = "            '{$fieldName}Key' => fn(\$record) => \$record->{$fieldName}->idkey()";
+                } else {
+                    $scheme[] = "            '$fieldName'";
+                }
+            }
+
+            if (!str_starts_with($fieldName, '_')) {
+                $required = $fieldMap['null'] || !is_null($fieldMap['default']) ? '->required(false)' : '';
+
+                $create = "        ";
+                $update = "        ";
+
+                switch ($fieldMap['type']) {
+                    case 'boolean':
+                        $create .= "\$input->fieldBool('$fieldName'){$required};";
+                        $update .= "\$input->fieldBool('$fieldName')->required(false);";
+                        break;
+                    case 'email':
+                        $create .= "\$input->field('$fieldName'){$required}->validate(FILTER_VALIDATE_EMAIL)->sanitize(FILTER_SANITIZE_EMAIL);";
+                        $update .= "\$input->field('$fieldName')->required(false)->validate(FILTER_VALIDATE_EMAIL)->sanitize(FILTER_SANITIZE_EMAIL);";
+                        break;
+                    case 'float':
+                        $create .= "\$input->field('$fieldName'){$required}->validate(FILTER_VALIDATE_FLOAT)->sanitize(FILTER_SANITIZE_NUMBER_FLOAT);";
+                        $update .= "\$input->field('$fieldName')->required(false)->validate(FILTER_VALIDATE_FLOAT)->sanitize(FILTER_SANITIZE_NUMBER_FLOAT);";
+                        break;
+                    case 'idx':
+                        $validate = prepare('fn($v) => \Model\[#model]\[#model]::$[#table]->getOneKey($v)->_checkInDb()', [
+                            'model' => strToPascalCase("db " . $fieldMap['settings']['datalayer']),
+                            'table' => strToCamelCase($fieldMap['settings']['table'])
+                        ]);
+
+                        $create .= "\$input->field('$fieldName'){$required}->validate($validate)->sanitize(fn(\$v) => idKeyId(\$v));";
+                        $update .= "\$input->field('$fieldName')->required(false)->validate($validate)->sanitize(fn(\$v) => idKeyId(\$v));";
+                        break;
+                    case 'int':
+                        $create .= "\$input->field('$fieldName'){$required}->validate(FILTER_VALIDATE_INT)->sanitize(FILTER_SANITIZE_NUMBER_INT);";
+                        $update .= "\$input->field('$fieldName')->required(false)->validate(FILTER_VALIDATE_INT)->sanitize(FILTER_SANITIZE_NUMBER_INT);";
+                        break;
+                    case 'json':
+                        $create .= "\$input->fieldScheme('$fieldName'){$required};";
+                        $update .= "\$input->fieldScheme('$fieldName')->required(false);";
+                        break;
+                    case 'md5':
+                    case 'mx5':
+                    case 'string':
+                    case 'text':
+                        $create .= "\$input->field('$fieldName'){$required};";
+                        $update .= "\$input->field('$fieldName')->required(false);";
+                        break;
+                    case 'time':
+                        $create .= "\$input->field('$fieldName'){$required}->sanitize(fn(\$v) => is_numeric(\$v) ? \$v : strtotime(\$v));";
+                        $update .= "\$input->field('$fieldName')->required(false)->sanitize(fn(\$v) => is_numeric(\$v) ? \$v : strtotime(\$v));";
+                        break;
+                }
+
+                if (!empty(trim($create)))
+                    $inputCreate[] = $create;
+                if (!empty(trim($update)))
+                    $inputUpdate[] = $update;
+            }
+        }
+
         $content = $this->template('class/class', [
             'key' => strToCamelCase("$tableName key"),
             'className' => $className,
             'table' => strToCamelCase($tableName),
             'model' => $model,
             'useModel' => "\\Model\\$model\\$model",
+            'scheme' => implode(",\n", $scheme),
+            'inputCreate' => implode("\n", $inputCreate),
+            'inputUpdate' => implode("\n", $inputUpdate),
         ]);
 
         $path = path($this->namespace);
 
-        File::create("class/$path/$className.php", $content);
+        File::create("class/$path/$className.php", $content, true);
     }
 
     protected function createRoute()
