48be40efa6d54f0ba8eecc6428a631442846d9ae
André Ricardo <contatoadmx@gmail.com>
2025-08-22 17:11:47 -0300

    show file in make command

--- system/terminal/make/command.php
+++ system/terminal/make/command.php
@@ -12,20 +12,20 @@ return new class extends Terminal {
         $command = remove_accents($command);
         $command = strtolower($command);
 
-        $commandFile = explode('.', $command);
-        $commandFile = array_map(fn($v) => strtolower($v), $commandFile);
-        $commandFile = path('system/terminal', ...$commandFile);
-        $commandFile = File::setEx($commandFile, 'php');
+        $file = explode('.', $command);
+        $file = array_map(fn($v) => strtolower($v), $file);
+        $file = path('system/terminal', ...$file);
+        $file = File::setEx($file, 'php');
 
-        if (File::check($commandFile))
+        if (File::check($file))
             throw new Exception("Command [$command] already exists in project");
 
         $template = Path::seekForFile('library/template/terminal/command.txt');
         $template = Import::content($template, ['command' => $command]);
 
-        File::create($commandFile, $template);
+        File::create($file, $template);
 
         self::echo('Command [[#]] created successfully', $command);
-        self::echo('[[#]]', $commandFile);
+        self::echo('[[#]]', $file);
     }
 };
