513ce5999474cc3ab543057cdfbe988cb6f16904
Anré Ricardo <contatoadmx@gmail.com>
2026-02-05 09:01:29 -0300

    fx installer

--- system/terminal/make/installer.php
+++ system/terminal/make/installer.php
@@ -18,6 +18,6 @@ return new class {
 
         File::create('install', $template);
 
-        Terminal::echol("File [#c:install] created successfully");
+        Terminal::echol("File [#c:p,install] created successfully");
     }
 };
