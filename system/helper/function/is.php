ab5d5e6b85cf164518afabbd34d3eacb0dc70998
Anré Ricardo <contatoadmx@gmail.com>
2026-02-09 07:05:48 -0300

    datalayer

--- system/helper/function/is.php
+++ system/helper/function/is.php
@@ -97,6 +97,19 @@ if (!function_exists('is_httpStatusError')) {
     }
 }
 
+if (!function_exists('is_idKey')) {
+
+    /** Verifica se uma variavel é um idKey */
+    function is_idKey(mixed $idKey): bool
+    {
+        if (Cif::check($idKey)) {
+            $idKey = Cif::off($idKey);
+            if (is_array($idKey) && is_string(array_shift($idKey)) && is_int(array_shift($idKey)))
+                return true;
+        }
+        return false;
+    }
+}
 
 if (!function_exists('is_image_base64')) {
 
