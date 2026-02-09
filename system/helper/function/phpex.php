3b3d98fbffe30bb495da537653d0db4b6cf101d4
Anré Ricardo <contatoadmx@gmail.com>
2026-02-09 07:12:16 -0300

    php ex

---
+++ system/helper/function/phpex.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('phpex')) {
+
+    /** Verifica se uma extensão do PHP está ativa */
+    function phpex(string $extension, bool $throw = false): bool
+    {
+        $loaded = extension_loaded($extension);
+
+        if (!$loaded && $throw)
+            throw new Exception("Extension [$extension] is required.", STS_INTERNAL_SERVER_ERROR);
+
+        return $loaded;
+    }
+}
