47a256f2c2d2d6dd53974a5a1c6d4478f0434424
Anré Ricardo <contatoadmx@gmail.com>
2026-02-12 10:05:31 -0300

    cls

--- class/PhpMx/Datalayer/Driver/Field/FTinyint.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace PhpMx\Datalayer\Driver\Field;
-
-class FTinyint extends FInt {}
--- system/helper/constant/request_is.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use PhpMx\Request;
-
-/** Indica se a execução atual está sendo feita via terminal CLI */
-define('IS_TERMINAL', PHP_SAPI == 'cli');
