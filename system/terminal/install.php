859551b7aca8eaf02a7be5ee9b31194d37acd9a1
Anré Ricardo <contatoadmx@gmail.com>
2026-02-21 14:46:31 -0300

    install and promote

---
+++ system/terminal/install.php
@@ -0,0 +1,36 @@
+<?php
+
+use PhpMx\Log;
+use PhpMx\Path;
+use PhpMx\Terminal;
+use PhpMx\Trait\TerminalInstallTrait;
+
+/**
+ * Executa os scripts de instalação de pacotes externos e atualiza o mapeamento do Composer.
+ */
+return new class {
+
+    function __invoke()
+    {
+        foreach (array_reverse(Path::seekForFiles('install')) as $installFile) {
+
+            $origin = Path::origin($installFile);
+
+            if ($origin != 'current-project') {
+                Log::add('mx', "Install [$origin]", function () use ($installFile, $origin) {
+                    ob_start();
+                    $script = require $installFile;
+                    ob_end_clean();
+
+                    if (is_trait($script, TerminalInstallTrait::class)) {
+                        Terminal::echol("Installing [#c:p,$origin]");
+                        $script();
+                        Terminal::echol();
+                    }
+                });
+            }
+        }
+
+        Terminal::run('composer 1');
+    }
+};
