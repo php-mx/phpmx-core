8250657c2dc0f76a6fa5231d227de35d731a7a62
Anré Ricardo <contatoadmx@gmail.com>
2026-02-21 21:08:24 -0300

    fx return helper.function

--- system/terminal/helper/function.php
+++ system/terminal/helper/function.php
@@ -24,7 +24,7 @@ return new class {
                 foreach ($item['description'] as $description)
                     Terminal::echol("      $description");
                 foreach ($item['variations'] as $variation)
-                    Terminal::echol("         [#name][#c:dd,(]{$variation}[#c:dd,):][#c:pd,#return]", $item);
+                    Terminal::echol("         [#name][#c:dd,(]{$variation}[#c:pd,#return]", $item);
             }
         );
     }
@@ -63,6 +63,7 @@ return new class {
 
                 $scheme['variations'] = $variations;
                 $scheme['sep'] = ',';
+                $scheme['return'] =  $scheme['return'] ? ":" . $scheme['return'] : '';
 
                 $items[] = $scheme;
             }
